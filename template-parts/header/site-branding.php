017f8bb1803fa5a814e5a15066248933b82bfd2e
Jeremy Clarke <jer@simianuprising.com>
2017-08-03 01:10:30 -0400

    add site-branding.php override to control the title

---
+++ template-parts/header/site-branding.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+/**
+ * Custom site-branding.php file so that we can filter this one use of the site name. 
+ * 
+ * Normally we'd filter the original get_bloginfo( 'name' ), but I only want to affect this instance
+ */
+$site_name = "<span class='logo-chunk simian-uprising'>Simian Uprising</span> <span class='logo-chunk by-jer-clarke'><span class='logo-chunk by'>by</span> <span class='logo-chunk jer-clarke'>Jer Clarke</span></span>"
+
+?>
+<div class="site-branding">
+	<div class="wrap">
+
+		<?php the_custom_logo(); ?>
+
+		<div class="site-branding-text">
+			<?php if ( is_front_page() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $site_name ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $site_name ?></a></p>
+			<?php endif; ?>
+
+			<?php
+			$description = get_bloginfo( 'description', 'display' );
+
+			if ( $description || is_customize_preview() ) :
+			?>
+				<p class="site-description"><?php echo $description; ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding-text -->
+
+		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
+	<?php endif; ?>
+
+	</div><!-- .wrap -->
+</div><!-- .site-branding -->
